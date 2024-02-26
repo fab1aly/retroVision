5d07c44d2391e3b218b719fca3cb6170f624b8ad
fab1aly <fabien.aly@gmail.com>
2024-02-26 22:24:19 +0100

    header & nav & menu

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class IndexController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('view/index.html.twig', [
+            'controller_name' => 'IndexController',
         ]);
     }
 }
--- src/Controller/VideosController.php
+++ src/Controller/VideosController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController extends AbstractController
+class VideosController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('/videos', name: 'app_videos')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->render('view/videos.html.twig', [
+            'controller_name' => 'VideosController',
         ]);
     }
 }
